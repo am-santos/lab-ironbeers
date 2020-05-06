0488564a70694cf260fb89a925cf30083f548f9b
am-santos <antopuppe@gmail.com>
2020-05-06 18:25:08 +0100

    Finished until iteration 6

--- app.js
+++ app.js
@@ -14,12 +14,52 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (request, response) => {
+  punkAPI
+    .getBeers()
+    .then(arrayOfBeers => {
+      console.log('getBeers is working');
+      response.render('beers', { beer: arrayOfBeers });
+    })
+    .catch(error => {
+      console.log('error on the getBeers method');
+      console.log(error);
+    });
+});
+
+app.get('/random-beer', (request, response) => {
+  punkAPI
+    .getRandom()
+    .then(randomBeer => {
+      console.log('getRandom is working');
+      response.render('random-beer', { beer: randomBeer[0] });
+    })
+    .catch(error => {
+      console.log('error on the getRandom method');
+      console.log(error);
+    });
+});
+
+app.get('/beers/:id', (request, response) => {
+  const id = request.params.id;
+  punkAPI
+    .getBeer(id)
+    .then(beer => {
+      console.log('getBeer is working');
+      response.render('singleBeer', { beer: beer[0] });
+    })
+    .catch(error => {
+      console.log('error on the getBeer method');
+      console.log(error);
+    });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
